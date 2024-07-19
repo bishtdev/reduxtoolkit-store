9ff7103cb8f78e92c7384eacc9b592a8e64ebea3
bishtdev <bishtdev030@gmail.com>
2024-07-19 16:53:56 +0530

    setting up the project

---
+++ src/Data.js
@@ -0,0 +1,50 @@
+export const storeData = [
+    {
+        name:'hustler' ,
+        price: 350,
+        image: 'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/628x301/9df78eab33525d08d6e5fb8d27136e95//l/i/orange-green-full-rim-wayfarer-lenskart-studio-lenskart-hustlr-la-e15417-c6-sunglasses_g_9713_09_26_23.jpg',
+        amount: 1,
+    },
+    {
+        name: 'chase',
+        price: 449,
+        image: 'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/628x301/9df78eab33525d08d6e5fb8d27136e95//v/i/Vincent-Chase-VC-S14459-C2-Sunglasses_G_6919.jpg',
+        amount: 1,
+    },
+    {
+        name: 'poalarised',
+        price: 555,
+        image: 'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/628x301/9df78eab33525d08d6e5fb8d27136e95//v/i/gold-brown-gradient-full-rim-aviator-vincent-chase-polarized-vintage-vc-s11075-c11-sunglasses_g_3377_6_02_22.jpg',
+        amount: 1,
+    },
+    {
+        name: 'sigma' ,
+        price: 499,
+        image: 'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/628x301/9df78eab33525d08d6e5fb8d27136e95//v/i/Black-Full-Rim-Square-Vincent-Chase--MIRAGE1VC-S11748-C8-Polorized-Sunglasses_vincent-chase-vc-s11748-c8-sunglasses_g_5574_218_02_2022.jpg',
+        amount: 1,
+    },
+    {
+        name: 'LA mafia',
+        price: 999 ,
+        image:'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/628x301/9df78eab33525d08d6e5fb8d27136e95//v/i/silver-black-grey-gradient-rimless-square-vincent-chase-met-effect-vc-s15397-c2-sunglasses_g_1010_02_02_23.jpg',
+        amount: 1,
+    },
+    {
+        name: 'bombay don',
+        price: 1299,
+        image: 'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/628x301/9df78eab33525d08d6e5fb8d27136e95//v/i/grey-transparent-full-rim-geometric-vincent-chase-style-cast-pc-vc-s13833-c2-sunglasses_vincent-chase-vc-s13833-c2-suglasses_suglasses_g_6203_28july23.jpg',
+        amount: 1, 
+    },
+    {
+        name: 'boomer basic',
+        price: 789 ,
+        image: 'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/628x301/9df78eab33525d08d6e5fb8d27136e95//v/i/Vincent-Chase-VC-S14457-C3-Sunglasses_G_7067.jpg',
+        amount: 1,
+    },
+    {
+        name: 'lagoon',
+        price: 689 ,
+        image: 'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/628x301/9df78eab33525d08d6e5fb8d27136e95//v/i/gunmetal-blue-full-rim-round-vincent-chase-polarized-the-fab-line-vc-s15749-c3-sunglasses_g_4593_07_07_23.jpg',
+        amount: 1,
+    },
+]
---
+++ src/features/basketSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    products: [],
+    amount:0,
+    total:0
+}
+
+const basketSlice = createSlice({
+    name:'basket',
+    initialState,
+})
+
